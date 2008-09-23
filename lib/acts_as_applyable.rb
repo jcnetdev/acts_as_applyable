85aacf1585dd3b7bb09dd61e4e8784e8674428b0
Jacques Crocker <jcnetdev@gmail.com>
2008-09-23 04:37:45 -0700

    Fixing up the default applyable fields

--- lib/acts_as_applyable.rb
+++ lib/acts_as_applyable.rb
@@ -2,19 +2,11 @@ module ActiveRecord
   module Acts
     module Applyable
       def self.included(base)
-        base.extend ClassMethods
         base.class_eval do
           include ActiveRecord::Acts::Applyable::InstanceMethods
         end
       end
-      
-      # module ClassMethods
-      #   def acts_as_applyable(*accessible_attributes)
-      #     cattr_accessor :accessible_attribute_list
-      #     self.accessible_attribute_list = accessible_attributes
-      #   end
-      # end
-      
+
       module InstanceMethods
         def params
           @params ||= {}
@@ -27,11 +19,19 @@ module ActiveRecord
         
         def apply_set_attributes(*attribute_list)
           self.params.stringify_keys!
-          @@accessible_attribute_list ||= []
           
+          # initialize attribute list if empty
+          if attribute_list.empty?
+            if self.respond_to?(:applyable_fields)
+              attribute_list = self.applyable_fields
+            else
+              Rails.logger.error("\n\n-------\n\nNO APPLYABLE FIELDS FOUND FOR #{self.class.to_s}\n"+
+                                 "Define a method called applyable_fields and return the array fields to apply by default\n\n-------\n\n")
+            end
+          end
+
           # build attributes hash
           attributes = {}
-          attribute_list = @@accessible_attribute_list if attribute_list.empty?
           attribute_list.each do |attribute|
             attributes[attribute] = self.params[attribute.to_s]
           end
